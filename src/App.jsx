7434da38cdded0580b644c1029547e9a6be49092
fixingman <cankilicbay@gmail.com>
2026-05-12 14:45:48 +0200

    v0.9.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { T, C } from "./tokens";
 import { uid } from "./data";
 import { useAppData, usePWA } from "./hooks";
